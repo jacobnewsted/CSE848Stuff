c972e9b43f4107f04b0b4207149c040f9de3dab0
Jacob Newsted <jnew2314@gmail.com>
2018-11-23 15:03:01 -0500

    Got rid of comments cause it is obvious I have no idea what I am doing

--- PyGAN/PyGAN.py
+++ PyGAN/PyGAN.py
@@ -16,14 +16,14 @@ def sample_noise(m,n):
     return np.random.uniform(-1., 1., size=[m,n])
 
 def generator(Z, hsize=[16,16], reuse=False):
-    with tf.variable_scope("GAN/Generator", reuse=reuse,dtype=tf.float16):
+    with tf.variable_scope("GAN/Generator", reuse=reuse):
         h1 = tf.layers.dense(Z, hsize[0], activation=tf.nn.leaky_relu)
         h2 = tf.layers.dense(h1, hsize[1], activation=tf.nn.leaky_relu)
         out = tf.layers.dense(h2, 2)
     return out
 
 def discriminator(X, hsize=[16,16], reuse=False):
-    with tf.variable_scope("GAN/Discriminator",reuse=reuse,dtype=tf.float16):
+    with tf.variable_scope("GAN/Discriminator",reuse=reuse):
         h1 = tf.layers.dense(X,hsize[0], activation=tf.nn.leaky_relu)
         h2 = tf.layers.dense(h1, hsize[1], activation=tf.nn.leaky_relu)
         h3 = tf.layers.dense(h2, 2)
@@ -31,61 +31,53 @@ def discriminator(X, hsize=[16,16], reuse=False):
     return out,h3
 
 
-batch_size = 256
-# 2D data with no specific shape represented by int16's
 X = tf.placeholder(tf.float32,[None,2])
 Z = tf.placeholder(tf.float32,[None,2])
 
-# Generate noise
 G_sample = generator(Z)
-r_logits, r_rep = discriminator(X) # Generate probabilities that the sample is real
-f_logits, g_rep = discriminator(G_sample, reuse=True) # Generate probabilities the sample is false from noise
+r_logits, r_rep = discriminator(X)
+f_logits, g_rep = discriminator(G_sample, reuse=True)
 
-# Yikes, we are calculating the total loss by summing the probability of it being real (mapped onto the tensor shape for the true logit only considering chances of it being real)
-# by the probability of it being false (mapped onto the tensor shape for the fake logit considering only chances that it is fake)
 disc_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=r_logits,labels=tf.ones_like(r_logits)) + tf.nn.sigmoid_cross_entropy_with_logits(logits=f_logits, labels=tf.zeros_like(f_logits)))
 gen_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = f_logits, labels=tf.ones_like(f_logits)))
-# Getting our optimizer for back-propogation of error
-gen_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Generator")
-disc_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,scope="GAN/Discriminator")
 
-# Most implementations use Adam but the example I am going off of uses a rectified one, doesn't matter much for this though
+gen_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope="GAN/Generator")
+disc_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope="GAN/Discriminator")
+
 gen_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(gen_loss,var_list=gen_vars)
 disc_step = tf.train.RMSPropOptimizer(learning_rate=0.001).minimize(disc_loss,var_list=disc_vars)
 
 sess = tf.Session()
 tf.global_variables_initializer().run(session=sess)
 
+batch_size = 256
 n_dsteps = 10
 n_gsteps = 10
 X_batch = []
 Z_batch = []
-for i in range(101):
+for i in range(1001):
     X_batch = sample_data()
     Z_batch = sample_noise(batch_size, 2)
     for j in range(n_dsteps):
         _, dloss = sess.run([disc_step, disc_loss], feed_dict={X: X_batch, Z: Z_batch})
-    rrep_dstep, grep_dstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
-    for j in range(n_gsteps):
+    for j in range(n_gsteps): 
         _, gloss = sess.run([gen_step, gen_loss], feed_dict={X: X_batch, Z: Z_batch})
-    rrep_gstep, grep_gstep = sess.run([r_rep, g_rep], feed_dict={X: X_batch, Z: Z_batch})
 
     print("Iterations: %d\t Discriminator loss: %.4f\t Generator loss: %.4f"%(i,dloss,gloss))
-    if i%10 == 0:
-        print("%d,%f,%f\n"%(i,dloss,gloss))
 
+Z_batch = sample_noise(batch_size, 2)
 result = sess.run(G_sample, feed_dict={Z: Z_batch})
 print("\nGenerated Data\n")
 for i in range(len(result)):
     if i%16 == 0:
         print("")
     else:
-        print(int(round(result[i][1])), sep=" ", end="", flush=True)
+        print(int(result[i][1]), sep=" ", end="", flush=True)
 
 print("\nReal Data\n")
 for i in range(len(sample_data())):
     if i%16 == 0:
         print("")
     else:
-        print(int(round(sample_data()[i][1])), sep=" ", end="", flush=True)
+        print(int(sample_data()[i][1]), sep=" ", end="", flush=True)
 print("")
